0a0f1a93f11f4e2d87f03111374d2008ac751a36
JersonMolina <molinajerson885@gmail.com>
2026-04-01 13:41:20 -0500

    se configura el docController

--- ui-service/src/main/java/com/brixo/slidehub/ui/controller/DocsController.java
+++ ui-service/src/main/java/com/brixo/slidehub/ui/controller/DocsController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DocsController {
 
+    @GetMapping("/ai-guide")
+    public String aiGuide() {
+        return "ai-guide";
+    }
 }
