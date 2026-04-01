51f76ecfc1696a2ced980b0a5e5b69697fedc1a7
JersonMolina <molinajerson885@gmail.com>
2026-04-01 13:38:04 -0500

    docs controller creado

---
+++ ui-service/src/main/java/com/brixo/slidehub/ui/controller/DocsController.java
@@ -0,0 +1,9 @@
+package com.brixo.slidehub.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DocsController {
+
+}
