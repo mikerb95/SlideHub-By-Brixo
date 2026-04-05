f05f893eba5b301b5221386c5d5bcbad5002eeaf
JersonMolina <molinajerson885@gmail.com>
2026-04-05 18:46:43 -0500

    se agrega controlador

---
+++ slidehub-monolith/src/main/java/com/brixo/slidehub/state/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.brixo.slidehub.state.controller;
+
+import com.brixo.slidehub.state.model.DemoState;
+import com.brixo.slidehub.state.model.SetDemoRequest;
+import com.brixo.slidehub.state.service.DemoStateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
